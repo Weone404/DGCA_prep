d22b16dcae95b88a275a3cba23726a52a5f31113
Sunder gupta <sunder1234gupta@gmail.com>
2026-08-25 14:51:15 +0530

    Remove root dashboard rewrite

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async rewrites() {
-    return {
-      beforeFiles: [
-        {
-          source: '/',
-          destination: '/dashboard',
-        },
-      ],
-    }
-  },
   async headers() {
     return [
       {
